004dd0b055ef4bf6de976be646663ae9a281cc1e
MaxIGL <73345951+MaxIGL@users.noreply.github.com>
2021-06-09 14:40:50 +0200

    Update occupancymap_planner_client.cpp

--- occupancymap_planner_client.cpp
+++ occupancymap_planner_client.cpp
@@ -5,18 +5,19 @@
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "occupancymap_planner");
-  if (argc != 4)
+  if (argc != 5)
   {
-    ROS_INFO("usage: occupancymap_planner file_in file_out resolution_discretized");
+    ROS_INFO("usage: occupancymap_planner file_in_map file_in_artag file_out resolution_discretized");
     return 1;
   }
 
   ros::NodeHandle n;
   ros::ServiceClient client = n.serviceClient<my_pcl_tutorial::occupancymap_planner>("occupancymap_planner");
   my_pcl_tutorial::occupancymap_planner srv;
-  srv.request.file_in = argv[1];
-  srv.request.file_out = argv[2];
-  srv.request.resolution_discretized = atof(argv[3]);
+  srv.request.file_in_map = argv[1];
+  srv.request.file_in_artag = argv[2];
+  srv.request.file_out = argv[3];
+  srv.request.resolution_discretized = atof(argv[4]);
   if (client.call(srv))
   {
     ROS_INFO("The call of the service occupancy map planner was a success");
@@ -29,3 +30,5 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
+
